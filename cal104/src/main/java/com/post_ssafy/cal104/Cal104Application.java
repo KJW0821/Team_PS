fc58f6a686bcf0871a3e7773a15b218bd22ef38e
KJW0821 <force1394@gmail.com>
2024-10-03 17:01:11 +0900

    chore: 프로젝트 초기 설정, gradle 빌드

---
+++ cal104/src/main/java/com/post_ssafy/cal104/Cal104Application.java
@@ -0,0 +1,13 @@
+package com.post_ssafy.cal104;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cal104Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cal104Application.class, args);
+	}
+
+}
---
+++ cal104/src/test/java/com/post_ssafy/cal104/Cal104ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post_ssafy.cal104;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cal104ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
